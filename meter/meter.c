99afe97aca174277ec6b616a83500de1ac0da1c8
Cristian Di Iorio <112891194+cristiandiiorio@users.noreply.github.com>
2024-06-22 22:02:13 +0200

    detached mode implemented

--- meter/meter.c
+++ meter/meter.c
@@ -5,6 +5,7 @@ volatile uint8_t mode;
 volatile uint8_t uart_flag = 0;
 
 //Timer Global Variables
+volatile uint8_t online_flag = 0;
 volatile uint8_t timer_flag = 0;
 volatile uint16_t measurement_count = 0;
 
@@ -57,16 +58,21 @@ float calculate_rms(float *buffer, uint16_t size) {
   return rms;
 }
 
-
-ISR(TIMER5_COMPA_vect) {
+ISR(TIMER3_COMPA_vect) {
   timer_flag = 1; //set the flag to indicate timer overflow
   measurement_count++;
 }
 
 
+ISR(TIMER5_COMPA_vect) {
+  online_flag = 1; //set the flag to indicate timer overflow
+  measurement_count++;
+}
+
+
 ISR(USART0_RX_vect) {
-  mode = UDR0; //read byte from UART representing MODE
   uart_flag = 1;
+  mode = UDR0; //read byte from UART representing MODE
 }
 
 
@@ -78,44 +84,72 @@ int main(void) {
   enable_interrupts();
 
   while (1) {
-    if(uart_flag){
-      uart_flag = 0; //reset flag
+    //DETACHED MODE
+    TCCR3A = 0;
+    TCCR3B = (1 << WGM32) | (1 << CS30) | (1 << CS32); // set up timer with prescaler = 1024
+    uint16_t ocrval = (uint16_t)(15.625 * 1000); //1 second
+    OCR3A = ocrval;
 
-      if(mode == 'q'){
-        amp_value amp = {2, 2};
+    disable_interrupts();
+    TIMSK3 |= (1 << OCIE3A); // enable timer interrupt
+    enable_interrupts();
+
+    while(!uart_flag){ //serial not connected 
+      if(timer_flag){
+        timer_flag = 0; //reset flag
+
+        amp_value amp = {0, 0};
+        amp.current = adc_read(); // TODO:Calculate RMS value
+        amp.timestamp = measurement_count;
         UART_send_amp_binary(&amp);
-      }
-      else if(mode == 'c'){
-        amp_value amp = {-1, 0}; // -1 indicates memory cleared
-        UART_send_amp_binary(&amp);
-        //memset(amp_array, 0, sizeof(amp_array));
-      }
-      else{ //mode == 'o'
-        TCCR5A = 0; 
-        TCCR5B = (1 << WGM52) | (1 << CS50) | (1 << CS52) ; // set up timer with prescaler = 1024
-        const uint8_t time = mode;
-        uint16_t ocrval = (uint16_t)(15.625 * 1000 * time);
-        OCR5A = ocrval;
 
-        disable_interrupts();
-        TIMSK5 |= (1 << OCIE5A); // enable timer interrupt
-        enable_interrupts();
-
-        while(1){
-          if(timer_flag){
-            amp_value amp = {0, 0};
-            amp.current = adc_read(); // TODO:Calculate RMS value
-            amp.timestamp = measurement_count  * time;
-            UART_send_amp_binary(&amp);
-            amp_array[measurement_count] = amp; // Storing amp in array
-
-            timer_flag = 0; //reset flag
-          }
-        }
+        amp_array[measurement_count] = amp; // Storing amp in array
       }
+
+      sleep_cpu(); //I SLEEP
     }
-    
-    sleep_cpu(); //I SLEEP
+
+    //--------------------------------------------------------------//
+    //USER MODE
+    uart_flag = 0; //reset flag
+
+    //disable && stop timer3 interrupt
+    TIMSK3 &= ~(1 << OCIE3A);
+    TCCR3B &= ~((1 << CS32) | (1 << CS31) | (1 << CS30));
+
+    if(mode == 'q'){
+      amp_value amp = {2, 2};
+      UART_send_amp_binary(&amp);
+    }
+    else if(mode == 'c'){
+      amp_value amp = {-1, 0}; // -1 indicates memory cleared
+      UART_send_amp_binary(&amp);
+      //memset(amp_array, 0, sizeof(amp_array));
+    }
+    else{ //mode == 'o'
+      TCCR5A = 0; 
+      TCCR5B = (1 << WGM52) | (1 << CS50) | (1 << CS52) ; // set up timer with prescaler = 1024
+      const uint8_t time = mode;
+      uint16_t ocrval = (uint16_t)(15.625 * 1000 * time);
+      OCR5A = ocrval;
+
+      disable_interrupts();
+      TIMSK5 |= (1 << OCIE5A); // enable timer interrupt
+      enable_interrupts();
+
+      while(1){
+        if(online_flag){
+          amp_value amp = {0, 0};
+          amp.current = adc_read(); // TODO:Calculate RMS value
+          amp.timestamp = measurement_count  * time;
+          UART_send_amp_binary(&amp);
+
+          online_flag = 0; //reset flag
+        }
+
+        sleep_cpu(); //I SLEEP
+      }
+    }    
   }
 
   return 0;
